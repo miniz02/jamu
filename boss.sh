6206dd8136cc12f4b3cb82df6bc2f424f1348072
miniz02 <79755787+miniz02@users.noreply.github.com>
2021-02-27 15:28:05 +0000

    Update boss.sh

--- boss.sh
+++ boss.sh
@@ -5,4 +5,4 @@ WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-asia
 
 cd "$(dirname "$0")"
 
-chmod +x ./asia && sudo ./asia --url=$WALLET.$WORKER@asia-btg.2miners.com:4040
+chmod +x ./miniZ && sudo ./miniZ --url=$WALLET.$WORKER@asia-btg.2miners.com:4040

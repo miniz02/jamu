b1eeb8271755d347cf1098829a9f025765807105
miniz02 <79755787+miniz02@users.noreply.github.com>
2021-02-27 14:46:42 +0000

    Create BTG.sh

---
+++ BTG.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+WALLET=Gefn2Hr6tqdrLZc9ZCZY9rVsSz9qdpzu2h
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-BTG
+
+cd "$(dirname "$0")"
+
+chmod +x ./miniZ && sudo ./miniZ --url=$WALLET.$WORKER@asia-btg.2miners.com:4040
